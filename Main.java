2a3f6c6cfffc63c1afbbca1ebdf722bcd0830e8a
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2021-05-27 18:00:26 -0600

    Programa v2

--- Main.java
+++ Main.java
@@ -11,16 +11,18 @@ public class Main {
         Matriz graph = new Matriz();
 
         open.add(graph);
-        graph.Floyd();
 
         int menu1=1;
         int menu2; 
         boolean loop = true;
 
-        while(loop){       
+        while(loop){   
+
+            graph.Floyd();
+            
             System.out.println("\n\n----MENU PRINCIPAL----");
             System.out.println("1. Buscar ruta mas corta entre dos ciudades ");
-            System.out.println("2. Centro del grafo *no implementado (bajo indicacion de Moises)*");
+            System.out.println("2. Centro del grafo");
             System.out.println("3. Modificar grafo");
             System.out.println("4. Mostrar grafo");
             System.out.println("5. Salir del programa");
@@ -37,20 +39,17 @@ public class Main {
                 
                 if(graph.contains(ciudad1,ciudad2)){
 
-                    System.out.println("\n\nLa distancia minima es: "+graph.getVertice(ciudad1, ciudad2)+".");
+                    System.out.println("\n\nLa distancia minima es: "+graph.distance(ciudad1, ciudad2)+".");
 
-                    if(graph.getVertice(ciudad1, ciudad2)!=10000){
-                        System.out.print("La ruta es: "+ciudad1);
-                        //graph.mostrarIntermedias(graph.getIndex(ciudad1), graph.getIndex(ciudad2));
-                        System.out.println(", "+ciudad2+"\n");
-                    }
+                    graph.between(ciudad1, ciudad2);
+                    
 
                 }else{
                     System.out.println("\nNo se ha encontrado alguna (o ambas) de las ciudades ingresadas. Intentar de nuevo."); 
                 }
             }
             if(menu1==2){
-                System.out.println("\nBajo indicaciones de Moises, esta parte fue eliminada de la rubrica/tarea."); 
+                
             }
 
             else if(menu1==3){
@@ -66,7 +65,9 @@ public class Main {
                     String ciudad2 = scan.next();
 
                     if(graph.contains(ciudad1,ciudad2)){
-                        graph.agregar(ciudad1, ciudad2, "10000");
+                        graph.add(ciudad1, ciudad2, "10000");
+                    }else{
+                        System.out.println("Una o ambas ciudades no exiten en el sistema."); 
                     }
                 }
 
@@ -78,13 +79,14 @@ public class Main {
                     System.out.println("Ingrese la distancia entre las ciudades");
                     String distancia = scan.next();
 
-                    graph.agregar(ciudad1, ciudad2, distancia);
+                    graph.add(ciudad1, ciudad2, distancia);
                     
                 } 
-                graph.Floyd();
+
             }
 
             if(menu1==4){
+                System.out.println("\n--MATRIZ--\n"); 
                 graph.show();
             }
 
--- Matriz.java
+++ Matriz.java
@@ -4,12 +4,14 @@ public class Matriz {
 
     private Vector<String> ciudades;
     private int[][] km;
+    private int[][] ciudades2;
     
 
     public Matriz(){
     
         ciudades = new Vector<String>();
         km = new int [30][30];
+        ciudades2 = new int [30][30];
 
         for(int i=0;i<30;i++){
             for(int j=0;j<30;j++){
@@ -19,30 +21,68 @@ public class Matriz {
                 else{
                     km[i][j]=10000;
                 }
+
+                ciudades2[i][j] = 10000;
+                
             }
         }
     }
 
     public void Floyd(){
-        for (int k = 0; k < 30; k++)
+        for (int k = 0; k<ciudades.size(); k++)
         {
-            // Pick all vertices as source one by one
-            for (int i = 0; i < 30; i++)
+
+            for (int i = 0; i<ciudades.size(); i++)
             {
-                // Pick all vertices as destination for the
-                // above picked source
-                for (int j = 0; j < 30; j++)
+
+                for (int j = 0; j<ciudades.size(); j++)
                 {
-                    // If vertex k is on the shortest path from
-                    // i to j, then update the value of dist[i][j]
-                    if (km[i][k] + km[k][j] < km[i][j])
+
+                    if (km[i][k] == 10000 || km[k][j] == 10000)
+                        continue;
+            
+                    if (km[i][k] + km[k][j] < km[i][j]){
                         km[i][j] = km[i][k] + km[k][j];
+                        ciudades2[i][j] = ciudades2[i][k];
+                    }
+                        
                 }
             }
         }
     }
 
-    public void agregar(String cd1, String cd2, String cant) {
+    public void between(String cd1, String cd2) {
+        int i = ciudades.indexOf(cd1);
+        int j = ciudades.indexOf(cd2);        
+        
+        printCities(i,j);
+        
+    }
+
+    private void printCities(int i, int j) {
+        Vector<String> path = new Vector<String>();
+        path.add("-> "+ciudades.get(i));
+
+        if (km[i][j] == 10000 || km[i][j] == 0){
+            System.out.println("No hay camino. Hay una interrupcion o el camino es muy largo.\n"); 
+        }else{
+            
+            while (i != j)
+            {
+                i = ciudades2[i][j];
+                path.add(ciudades.get(i));
+            }
+
+        }
+
+        int n = path.size();
+        for(int l = 0; l < n - 1; l++)
+        System.out.print(path.get(l) + " -> ");
+        System.out.print(path.get(n - 1) + "\n");
+    }
+
+
+    public void add(String cd1, String cd2, String cant) {
         
         if(!ciudades.contains(cd1)){
             ciudades.add(cd1);
@@ -54,19 +94,36 @@ public class Matriz {
         int i = ciudades.indexOf(cd1);
         int j = ciudades.indexOf(cd2);        
         km[i][j]= Integer.parseInt(cant);
+
+        if(cant == "10000"){
+            ciudades2[i][j] = 10000;
+        }else{
+            ciudades2[i][j] = j;
+        }
         
     }
 
     public void show(){
+        System.out.println("\n--MATRIZ DE DISTANCIAS ENTRE NODOS (KM)--\n");
         for(int a=0;a<ciudades.size();a++){
             for(int b=0;b<ciudades.size();b++){
                 System.out.print(""+km[a][b]+" ");
             }
             System.out.println("");
         }
+
+        System.out.println("\n--MATRIZ DE CAMINOS--\n");
+        for(int a=0;a<ciudades.size();a++){
+            for(int b=0;b<ciudades.size();b++){
+                System.out.print(""+ciudades2[a][b]+" ");
+            }
+            System.out.println("");
+        }
     }
 
-    public int getVertice(String cd1, String cd2){
+    //------------------METODOS EXTRA-----------------------
+
+    public int distance(String cd1, String cd2){
         return km[ciudades.indexOf(cd1)][ciudades.indexOf(cd2)];
     }
 
@@ -81,7 +138,6 @@ public class Matriz {
         }
         return status;
     }
-
     
 }
 
